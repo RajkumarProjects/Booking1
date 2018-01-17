a0d5539061c3d226e4cc7159ad3246fa53673b24
Rajkumar <rajkumaar.rajendran@gmail.com>
2018-01-17 12:27:26 +0000

    Master Changes

---
+++ 15012018/Bookings/BookingUI/src/app/back.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Back extends React.Component {
+    constructor(props) {
+        super(props);             
+    }
+    render() {
+ 
+        return (
+            <a onClick={this.onBackClick.bind(this)} href='#'>Back to List</a>
+        );
+
+
+    }
+    onBackClick () {        
+        this.props.onBackClick();
+    }
+}
+module.exports = Back;
